b713029a4fd972ead337e313f14dacebe8bfa162
Zeynep Alıcı <aliciizeynep@gmail.com>
2021-12-22 01:50:37 +0300

    add client, add pickle

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import pickle
+
+TCP_PORT = 2425
+UDP_PORT = 4242
+HEADER_LENGTH = 16
+FORMAT = 'utf-8'
+SERVER = socket.gethostbyname(socket.gethostname())  # getting local ip automatically
+ADDR = (SERVER, TCP_PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg, header_message):
+    # message = msg.encode(FORMAT)
+    message = pickle.dumps(msg)
+    message = bytes(f'{header_message:<{HEADER_LENGTH}}', FORMAT) + bytes(f'{len(message):<{HEADER_LENGTH}}',
+                                                                          FORMAT) + message
+
+    client.send(bytes(message))
+    print(client.recv(2048).decode(FORMAT))
+
+
+dictionary = {'username': 'zeynep', 'password': '123'}
+send(dictionary, 'REGISTER')
+
--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import socket
 import threading
+import pickle
 from tcp_socket import TCP_Socket
 from udp_socket import UDP_Socket
 from util import *
 from user import User
 
 HEADER_LENGTH = 16
-HEADERS = ['LOGIN', 'LOGOUT', 'HELLO', 'CHATREQUEST', 'OK', 'REJECT', 'BUSY'] # We can use this by enumerating 
-IP = '127.0.0.1'
-TCP_PORT = 2424
+HEADERS = ['REGISTER', 'LOGIN', 'LOGOUT', 'HELLO', 'CHATREQUEST', 'OK', 'REJECT',
+           'BUSY']  # We can use this by enumerating
+IP = socket.gethostbyname(socket.gethostname())
+TCP_PORT = 2425
 UDP_PORT = 4242
 
 tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,7 +22,28 @@ udp_sock.bind((IP, UDP_PORT))
 clients = []
 
 
-def receive_packet(client_socket: socket.socket):
+def register(packet_data, client_socket, client_address):
+    username = packet_data['username']
+    password = packet_data['password']
+
+    with threading.Lock():
+        if not check_user(clients, username):
+            user = User(username, password, client_address, status=True)
+            clients.append(user)
+            client_socket.send(b'The registration is complete.')
+        else:
+            client_socket.send(b'This username has already used.')
+
+
+def login(packet_data, client_socket,client_address):
+    pass
+
+
+def logout(packet_data, client_socket,client_address):
+    pass
+
+
+def receive_packet(client_socket, client_address):
     """
     Receive a packet from a client socket. Return header and data of the packet
     Args:
@@ -30,37 +53,45 @@ def receive_packet(client_socket: socket.socket):
      - Packet Header
      - Packet data
     """
-    try:
-        # First Header length part of package contains the header and the second part contains the packet length
-        packet_header = client_socket.recv(HEADER_LENGTH)
-        if packet_header not in HEADERS:
-            client_socket.send(b'Invalid header. Closing connection!')
-            client_socket.close()
-        
-        packet_length = client_socket.recv(HEADER_LENGTH)
-        packet_length = int(packet_length.decode('utf-8').strip())
-        packet_data = client_socket.recv(packet_length)
-        return packet_header.decode('utf-8'), packet_data
-    except:
-        return False
+    connected = True
+
+    while connected:
+        try:
+            # First Header length part of package contains the header and the second part contains the packet length
+            packet_header = client_socket.recv(HEADER_LENGTH)
+            packet_header = packet_header.decode('utf-8').rstrip(' ')
+            if packet_header not in HEADERS:
+                client_socket.send(b'Invalid header. Closing connection!')
+                connected = False
+                client_socket.close()
+
+            packet_length = client_socket.recv(HEADER_LENGTH)
+            packet_length = int(packet_length.decode('utf-8').strip())
+            packet_data = client_socket.recv(packet_length)
+            packet_data = pickle.loads(packet_data)
+            print(packet_data)
+
+            if packet_header == 'REGISTER':
+                register(packet_data, client_socket, client_address)
+            elif packet_header == 'LOGIN':
+                login(packet_data, client_socket, client_address)
+            elif packet_header == 'LOGOUT':
+                logout(packet_data, client_socket, client_address)
+            else:
+                pass
+
+        except:
+            return False
 
 
 # listen on the tcp socket
 def listen_tcp():
-    tcp_socket = TCP_Socket(clients, tcp_sock)
+    tcp_sock.listen()
+    print(f"[LISTENING] Server is listening on {IP, TCP_PORT}")
     while True:
-        client_socket, client_address = tcp_socket.socket.accept()
-        packet_header, packet_data = receive_packet(client_socket)
-
-
-        # This is a risky implementation because idk what will happen when more than one client sends packets at the same time.
-        # This is done because i didn't want to use switch case to call methods for headers
-        tcp_socket.client_address = client_address
-        tcp_socket.client_socket = client_socket
-        tcp_socket.packet_data = packet_data
-
-        header_method = getattr(tcp_socket, packet_header.lower())
-        result = header_method()
+        client_socket, client_address = tcp_sock.accept()
+        thread = threading.Thread(target=receive_packet, args=(client_socket, client_address))
+        thread.start()
 
 
 # listen on a udp socket
@@ -71,14 +102,13 @@ def listen_udp():
         header = data.decode('utf-8')
 
         header_method = getattr(udp_socket, header.lower())
-        user : User = check_user_fromip(udp_socket.clients, client_address)
+        user: User = check_user_fromip(udp_socket.clients, client_address)
         active_thread = threading.Thread(target=user.active)
         active_thread.start()
 
-        
+
 tcp_listener = threading.Thread(target=listen_tcp)
 tcp_listener.start()
 
 udp_listener = threading.Thread(target=listen_udp)
 udp_listener.start()
-
