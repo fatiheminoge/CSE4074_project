b4b023983e9ad206aa5e272a6cc9ec8ee726c337
Zeynep Alıcı <aliciizeynep@gmail.com>
2021-12-23 16:08:23 +0300

    add search

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from user import User
 
+
 def parse_msg():
     pass
 
@@ -8,10 +9,11 @@ def check_user_by_client_address(users, client_address):
     user = next((x for x in users if x.client_address == client_address), None)
     return user
 
-def check_user(users : list, username : str):
+
+def check_user(users: list, username: str):
     user = next((x for x in users if x.username == username), None)
     return user
 
-def check_password(user : User, password : str):
-    return user.password == password
 
+def check_password(user: User, password: str):
+    return user.password == password
